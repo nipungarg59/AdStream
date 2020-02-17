dac7cb885d17a4da268b5ce2341983e2c68155f6
Nipun Garg <nipun.garg@flipkart.com>
2020-02-18 01:12:34 +0530

    Adds frontend

---
+++ precache-manifest.bc8b48e668f457d5599e8fbc06e1190a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4dffaf8933931a75bd2bfdc000ef39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1303462407cf73474058",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d1363197979d548b2eb9",
+    "url": "/static/css/main.9f88da05.chunk.css"
+  },
+  {
+    "revision": "1303462407cf73474058",
+    "url": "/static/js/2.278476c2.chunk.js"
+  },
+  {
+    "revision": "9c62e788fd133d8bad7f91030fd7737c",
+    "url": "/static/js/2.278476c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1363197979d548b2eb9",
+    "url": "/static/js/main.8930969f.chunk.js"
+  },
+  {
+    "revision": "7674b0dfd168c9d68019",
+    "url": "/static/js/runtime-main.2416fd7b.js"
+  },
+  {
+    "revision": "0da7fbe3cc43699addea0b7929aca7f5",
+    "url": "/static/media/audio.0da7fbe3.jpg"
+  },
+  {
+    "revision": "1ec6ba937cd22e4de7528505b66dcac1",
+    "url": "/static/media/auto.1ec6ba93.jpg"
+  },
+  {
+    "revision": "6c00d04c0c59643c958663ccbb4a30a6",
+    "url": "/static/media/billboard.6c00d04c.jpg"
+  },
+  {
+    "revision": "14ae04067b57507f392204a798dfb4d7",
+    "url": "/static/media/bus.14ae0406.jpg"
+  },
+  {
+    "revision": "165a8d2328c395df80d252af7502ab30",
+    "url": "/static/media/car.165a8d23.jpg"
+  },
+  {
+    "revision": "9f57893512b303185f3d536e3a508a9e",
+    "url": "/static/media/carss.9f578935.jpg"
+  },
+  {
+    "revision": "5d7533df74647ce0b5a44770ff3b29e2",
+    "url": "/static/media/metro.5d7533df.jpg"
+  },
+  {
+    "revision": "9baa8dfbdacf7a7dfaa0659348f8d8ea",
+    "url": "/static/media/nbillboard.9baa8dfb.jpg"
+  }
+]);
